f69dcbe112f8bbf6449a1fa9dcd90bfce229a544
Josilene Vitoria dos Santos da Silva <josilenevitoriasilva@gmail.com>
2022-06-14 15:47:01 -0400

    feat: create product

--- src/http/graphql/resolvers/products.resolver.ts
+++ src/http/graphql/resolvers/products.resolver.ts
@@ -1,6 +1,9 @@
-import { Query, Resolver } from '@nestjs/graphql';
+import { UseGuards } from '@nestjs/common';
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
+import { AuthorizationGuard } from 'src/http/auth/authorization.guard';
 
 import { ProductsService } from 'src/services/products.service';
+import { CreateProductInput } from '../inputs/create-product-input';
 import { Product } from '../models/product';
 
 @Resolver()
@@ -8,8 +11,13 @@ export class ProductsResolver {
   constructor(private productsService: ProductsService) {}
 
   @Query(() => [Product])
-  // @UseGuards(AuthorizationGuard)
   products() {
     return this.productsService.listAllProducts();
+  }
+
+  @UseGuards(AuthorizationGuard)
+  @Mutation(() => Product)
+  createProduct(@Args('data') data: CreateProductInput) {
+    return this.productsService.createProduct(data);
   }
 }
